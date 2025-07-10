abb16003911df5786dfed51f3a92af27718b524c
Dharamveer06 <dharamveervadoliya@gmail.com>
2025-07-10 21:05:01 +0530

    Update model.py

--- model.py
+++ model.py
@@ -8,8 +8,8 @@ import io
 
 # Load the model
 import tensorflow as tf
+model = tf.keras.models.load_model("keras_model.h5")
 
-model = tf.keras.models.load_model(".keras")
 print(model.summary())
 
 
